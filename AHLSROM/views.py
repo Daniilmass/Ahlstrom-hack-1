a35ca5ac2423317525ce01e19b93086016bc7397
nostrumo <45200960+nostrumo@users.noreply.github.com>
2018-11-20 22:05:03 +0200

    Added base.html

--- AHLSROM/views.py
+++ AHLSROM/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
+import datetime
+
+def penis(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return render(request, 'test.html')
+
+def test(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return render(request, 'test.html')
 
 def index(request):
     return render(request, 'index.html')
