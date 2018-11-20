93d14e0fe78f9bcd9f0811789614eba10b02b334
mishgunnumberone <omibo006@edu.xamk.fi>
2018-11-20 17:21:11 +0200

    Added models

--- AHLSROM/models.py
+++ AHLSROM/models.py
@@ -2,3 +2,31 @@ from django.db import models
 
 # Create your models here.
 
+class News(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.CharField(max_length=200)
+    dateTime = models.CharField(max_length=50)
+    topic = models.CharField(max_length=60)
+
+class SpareParts(models.Model):
+    id = models.AutoField(primary_key=True)
+    counter = models.IntegerField()
+    name = models.CharField(max_length=60)
+
+
+class Machines(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=60)
+    parts = models.ManyToManyField(SpareParts)
+
+class Warehouse(models.Model):
+    id = models.AutoField(primary_key=True)
+    parts_id = models.ManyToManyField(SpareParts)
+
+class Issue(models.Model):
+    id = models.AutoField(primary_key=True)
+    machine_id = models.ManyToManyField(Machines)
+    parts_id = models.ManyToManyField(SpareParts)
+    time_not_working = models.CharField()
+    injury = models.BooleanField(default=False)
+    fixed = models.BooleanField(default=False)
