a1f3ecceb6a8c539da9755662e57a402b354d0d5
mishgunnumberone <omibo006@edu.xamk.fi>
2018-11-20 21:05:23 +0200

    Updated index.html with initial navigation bar

--- Django/urls.py
+++ Django/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from AHLSROM import views
 
 urlpatterns = [
+    path("", views.index),
     path('admin/', admin.site.urls),
 ]
