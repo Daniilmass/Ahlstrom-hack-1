b18fa2e006a504105f4bba2731f3139dffdd86fc
nostrumo <45200960+nostrumo@users.noreply.github.com>
2018-11-20 22:02:06 +0200

    Added admin panel mange

--- AHLSROM/views.py
+++ AHLSROM/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-import datetime
-
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+from django.http import HttpResponse, HttpResponseNotFound
 
 def index(request):
     return render(request, 'index.html')
