2fd95d8a31d07ba336273209ad0ac9ae3585e801
nostrumo <45200960+nostrumo@users.noreply.github.com>
2018-11-22 01:28:23 +0200

    Added redirect to exactly value in Parts

--- static/script.js
+++ static/script.js
@@ -5,4 +5,18 @@ $(document).ready(function() {
     $('#add_button').click(function() {
         $(location).attr('href',  location.href +'add/');
     });
+    $('#send_issue').click(function() {
+        var injury = $('#injury').is(':checked');
+        var fixed = $('#fixed').is(':checked');
+        var select_machine = $('#select_machine').val();
+        console.log(injury,fixed,select_machine)
+        $.ajax({
+          url: location.href,
+          type: "POST",
+          data: JSON.stringify( {'injury':injury,
+                 'fixed': fixed,
+                 'machines':[select_machine]}),
+          dataType: 'json'
+        });
+    });
 });
